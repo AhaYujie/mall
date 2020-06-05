d0eb550d7bae9cde50ca2b0d43d9db45f920b288
aha <1092739233@qq.com>
2020-06-05 19:52:46 +0800

    feat: 添加DO对象基类

---
+++ mall-common/src/main/java/online/ahayujie/mall/common/bean/model/Base.java
@@ -0,0 +1,56 @@
+package online.ahayujie.mall.common.bean.model;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.util.Date;
+
+/**
+ * 数据对象基类
+ * @author aha
+ * @date 2020/6/4
+ */
+public class Base {
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    @ApiModelProperty(value = "更新时间")
+    private Date updateTime;
+
+    @ApiModelProperty(value = "创建时间")
+    private Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Base{" +
+                "id=" + id +
+                ", updateTime=" + updateTime +
+                ", createTime=" + createTime +
+                '}';
+    }
+}
