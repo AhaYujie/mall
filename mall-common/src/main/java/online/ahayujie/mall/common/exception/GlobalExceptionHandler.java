f3ada2256c0d9d7ea47a9bc94e6af2c0dd98ddc8
aha <1092739233@qq.com>
2020-06-05 20:32:13 +0800

    feat: 添加对MethodArgumentNotValidException异常的处理

--- mall-common/src/main/java/online/ahayujie/mall/common/exception/GlobalExceptionHandler.java
+++ mall-common/src/main/java/online/ahayujie/mall/common/exception/GlobalExceptionHandler.java
@@ -2,11 +2,20 @@ package online.ahayujie.mall.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import online.ahayujie.mall.common.api.Result;
+import online.ahayujie.mall.common.api.ResultCodeEnum;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 全局异常处理
@@ -26,6 +35,20 @@ public class GlobalExceptionHandler {
             return Result.fail(e.getResultCode());
         }
         return Result.fail(e.getMessage());
+    }
+
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Result<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        List<String> messages = new ArrayList<>();
+        if (bindingResult.hasErrors()) {
+            for (ObjectError error : bindingResult.getAllErrors()) {
+                messages.add(error.getDefaultMessage());
+            }
+        }
+        return Result.fail(ResultCodeEnum.BAD_REQUEST, messages.toString());
     }
 
 }
