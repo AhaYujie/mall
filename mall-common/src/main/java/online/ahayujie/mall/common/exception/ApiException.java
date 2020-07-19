bef0da4859a19a9b390ee6152e603561408f996f
aha <1092739233@qq.com>
2020-07-19 15:09:17 +0800

    fix: 修复API异常父类构造器中没有初始化ResultCode导致NPE的BUG

--- mall-common/src/main/java/online/ahayujie/mall/common/exception/ApiException.java
+++ mall-common/src/main/java/online/ahayujie/mall/common/exception/ApiException.java
@@ -1,6 +1,7 @@
 package online.ahayujie.mall.common.exception;
 
 import online.ahayujie.mall.common.api.ResultCode;
+import online.ahayujie.mall.common.api.ResultCodeEnum;
 
 /**
  * API异常
@@ -9,23 +10,39 @@ import online.ahayujie.mall.common.api.ResultCode;
  */
 public class ApiException extends RuntimeException {
 
-    private ResultCode resultCode;
+    private final ResultCode resultCode;
 
     public ApiException(ResultCode resultCode) {
         super(resultCode.getMessage());
         this.resultCode = resultCode;
     }
 
+    /**
+     * {@link ResultCode} 默认为 {@code online.ahayujie.mall.common.api.ResultCodeEnum.FAIL}
+     * @param message 错误信息
+     */
     public ApiException(String message) {
         super(message);
+        this.resultCode = ResultCodeEnum.FAIL;
     }
 
+    /**
+     * {@link ResultCode} 默认为 {@code online.ahayujie.mall.common.api.ResultCodeEnum.FAIL}
+     * @param cause 错误原因
+     */
     public ApiException(Throwable cause) {
         super(cause);
+        this.resultCode = ResultCodeEnum.FAIL;
     }
 
+    /**
+     * {@link ResultCode} 默认为 {@code online.ahayujie.mall.common.api.ResultCodeEnum.FAIL}
+     * @param message 错误信息
+     * @param cause 错误原因
+     */
     public ApiException(String message, Throwable cause) {
         super(message, cause);
+        this.resultCode = ResultCodeEnum.FAIL;
     }
 
     public ResultCode getResultCode() {
