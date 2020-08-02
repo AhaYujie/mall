65fae07706078475c68523ed3a5e820b0fd49333
aha <1092739233@qq.com>
2020-08-02 21:08:32 +0800

    feat: 添加spring-boot-starter-data-redis依赖和redis配置

---
+++ mall-admin/src/main/java/online/ahayujie/mall/admin/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package online.ahayujie.mall.admin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
+
+/**
+ * @author aha
+ * @since 2020/7/31
+ */
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, Serializable> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(factory);
+        return redisTemplate;
+    }
+}
