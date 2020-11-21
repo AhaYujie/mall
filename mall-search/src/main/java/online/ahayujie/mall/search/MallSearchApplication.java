5be8616bc10fec69756ea43423baa4e3b768a3ff
aha <1092739233@qq.com>
2020-11-21 20:32:56 +0800

    feat: 修改Spring包扫描路径

--- mall-search/src/main/java/online/ahayujie/mall/search/MallSearchApplication.java
+++ mall-search/src/main/java/online/ahayujie/mall/search/MallSearchApplication.java
@@ -2,12 +2,14 @@ package online.ahayujie.mall.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author aha
  * @since 2020/10/25
  */
 @SpringBootApplication
+@ComponentScan(basePackages = {"online.ahayujie.mall"})
 public class MallSearchApplication {
     public static void main(String[] args) {
         SpringApplication.run(MallSearchApplication.class, args);
